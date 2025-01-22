d5de629d0b06466dab858186db811ae088f7077f
Emm4nuell <eduardo.eesf@gmail.com>
2025-01-22 07:58:43 -0300

    Criar projeto cgcreative

---
+++ src/main/java/br/com/cgcreative/CgcreativeApplication.java
@@ -0,0 +1,13 @@
+package br.com.cgcreative;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CgcreativeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CgcreativeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/cgcreative/CgcreativeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cgcreative;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CgcreativeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
